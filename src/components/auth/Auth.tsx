396a911e0ef62477c68fd437edebd06537c7d934
RuslanGnlln <92736615+RuslanGnlln@users.noreply.github.com>
2022-05-21 10:25:29 +0200

    Add files via upload

--- src/components/auth/Auth.tsx
+++ src/components/auth/Auth.tsx
@@ -1,11 +1,72 @@
-import React from 'react';
+import React, {FormEventHandler, useState} from 'react';
+import logo from './logo.svg';
+import './Auth.css';
+import {Link} from 'react-router-dom'
+
 
 const Auth = () => {
+
+    const [login, setLogin] = useState("")
+    const [password, setPassword] = useState("")
+
+    const submit = (e: React.FormEvent<HTMLFormElement>): void => {
+        e.preventDefault();
+        let request = {
+            method: 'POST',
+            headers: {'content-Type': 'application/json'},
+            body: JSON.stringify({login: login, password: password})
+        }
+
+
+    }
+
     return (
-        <div>
-            Test
+        <div className="wrapper">
+            <div className="main-container">
+                <div className="logo">
+                    <img src={logo} alt="logo"/>
+                    <div className="logo-text">
+                        *228*
+                    </div>
+                </div>
+
+                <div className="frame">
+                    <div className="text-auth">
+                        Вход
+                    </div>
+                    <div className="form-auth">
+                        <div className="inner-form-auth">
+                            <div className="input-form">
+                                <p>
+                                    Номер телефона / почта
+                                </p>
+                                <input className="input-frame" placeholder="Введите телефон или почту"
+                                       value={login} onChange={(event => {setLogin(event.target.value);})}
+                                />
+                            </div>
+                            <div className="input-form">
+                                <p>
+                                    Пароль
+                                </p>
+                                <input className="input-frame" placeholder="Введите пароль" type="password"
+                                    value={password} onChange={(event => {setPassword(event.target.value);})}
+                                />
+                            </div>
+                        </div>
+                        <button className="enter-button">Войти</button>
+                        <div>
+                            <label>
+                                Еще нет аккаунта?
+                            </label>
+                            <label>
+                                <a href="/registration">Зарегистрироваться</a>
+                            </label>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
-};
+}
 
 export default Auth;
